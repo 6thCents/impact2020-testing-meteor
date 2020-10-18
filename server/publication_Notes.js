64ca0f9c3e7a70be3f7672b70681fffc9a81a65e
Dave Rollins <drollins@sdcsol.com>
2020-10-17 20:59:43 -0400

    clean up

--- server/publication_Notes.js
+++ server/publication_Notes.js
@@ -2,6 +2,5 @@ import { Meteor } from 'meteor/meteor'
 import Notes from '../lib/notes'
 
 Meteor.publish('allnotes', function () {
-    console.log('getting all notes')
     return Notes.find({})
 });
